2478e672b6a713068debbe90325ec33ef897c39c
Ryan Grandgenett <ryan.grandgenett@gmail.com>
2015-10-10 13:00:28 -0500

    Fixed up application controller

--- app/controllers/challenges.js
+++ app/controllers/challenges.js
@@ -2,10 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   applicationController: Ember.inject.controller('application'),
-<<<<<<< HEAD
-  challenge: 'challenge string',
-=======
->>>>>>> e60b50d5f7fdbf86e8229e5fe428999d4f26389c
   isShowingChallengeModal: false,
   actions: {
     openLoginModal: function() {
