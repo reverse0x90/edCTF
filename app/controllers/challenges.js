e60b50d5f7fdbf86e8229e5fe428999d4f26389c
x64x6a <darekjens@gmail.com>
2015-10-10 02:14:17 -0500

    Updated scoreboard

--- app/controllers/challenges.js
+++ app/controllers/challenges.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   applicationController: Ember.inject.controller('application'),
-  challenge: '',
   isShowingChallengeModal: false,
   actions: {
     login: function(authenticationData) {
@@ -10,10 +9,10 @@ export default Ember.Controller.extend({
       this.sendAction('login', authenticationData);
     },
     openLoginModal: function() {
-      this.get('applicationController').set('isShowingLoginModal', true)
+      this.get('applicationController').set('isShowingLoginModal', true);
     },
     openChallenge: function(challenge_id) {
-        this.set('challenge', this.store.find('challenge', challenge_id)),
+        this.set('challenge', this.store.find('challenge', challenge_id));
         this.set('isShowingChallengeModal', true);
     },
   }
--- app/controllers/scoreboard.js
+++ app/controllers/scoreboard.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  sortedTeams: Ember.computed.sort('content', function(a, b){
+    if (a.get('points') < b.get('points')) {
+      return 1;
+    } else if (a.get('points') > b.get('points')) {
+      return -1;
+    }
+    return 0;
+  }),
+  numberTopTeams: 0,
+  topTeams: [],
+  actions:{
+    setTopTeams: function(n){
+      if (!n){
+        n = 10;
+      }
+      this.set('numberTopTeams', n);
+      this.set('topTeams', this.get('sortedTeams').slice(0,n));
+    },
+  },
 });
