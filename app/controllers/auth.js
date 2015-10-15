0db2f20607d49dd339f4e5a045a31779d75d182c
Ryan Grandgenett <ryan.grandgenett@gmail.com>
2015-10-15 16:28:21 -0500

    Added support for authenticated routes

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -2,10 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   isAuthenticated: false,
-  login: function(credentials){
+  currentTransition: null,
+  whiteList: ['index', 'scoreboard', 'about'],
+  inwhiteList: function(string){
     // Do the authentication stuff here
+    if ( this.get('whiteList').indexOf(string)>=0 ) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  },
+  login: function(credentials){
+    var t = this;
+    var currentTransition = this.get('currentTransition');
     credentials = null;
-    this.set('isAuthenticated', true);
+    t.set('isAuthenticated', true);
+    if ( currentTransition ) {
+      console.log("redirecting to: ", currentTransition.targetName)
+      t.set('currentTransition', null) 
+      currentTransition.retry();
+    }
   },
   register: function(registrationData){
     // Do the registration stuff here
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel: function(transition){
+    this.authCheck(transition);
+    //will have other stuff here once its connected to restapi
+  },
+  authCheck: function(transition){
+    //Method to check user credentials and redirect if necessary
+    console.log('Checking authentication');
+    var t = this;
+    var auth = t.controllerFor('auth');
+    var modal = t.controllerFor('modal');
+    console.log("targetName in application route:", transition.targetName);
+    if (!auth.inwhiteList(transition.targetName)) {
+      if(!auth.isAuthenticated){
+        auth.set('currentTransition', transition);
+        transition.abort();
+        console.log('User is unauthenicated, redirecting');
+        modal.set('modal.isLogin', true);  
+        t.transitionTo('index');
+      }
+    }
+  },
   setupController: function (controller, model){
     controller.set('authController', this.controllerFor('auth'));
     controller.set('modal', this.controllerFor('modal').get('modal'));
-  },
+  },  
+  actions: {
+    willTransition: function(transition){
+      console.log("targetName:", transition.targetName);
+      this.authCheck(transition);
+    }
+  }
 });
