f45121c2d2e60dd24461a31fa1fb645d8cc41240
x64x6a <darekjens@gmail.com>
2015-11-06 20:30:10 -0600

    Added python db sync

--- build.sh
+++ build.sh
@@ -9,3 +9,7 @@ cd ${EDCTF_DIR}/ember \
   && mv ${EDCTF_STATIC_DIR}/ember/index.html ${EDCTF_DIR}/edctf/api/templates/index.html \
   && cp -R ${DJANGO_STATIC_DIR}/admin ${EDCTF_STATIC_DIR}/admin \
   && cp -R /usr/local/lib/python2.7/dist-packages/rest_framework/static/rest_framework ${EDCTF_STATIC_DIR}/rest_framework
+
+python manage.py syncdb
+
+sudo chown $USER:www-data *.sqlite3
