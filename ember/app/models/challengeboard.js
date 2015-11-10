305abc4c69120f45778bf988461106a50cfa9c0e
x64x6a <darekjens@gmail.com>
2015-11-10 14:49:14 -0800

    Updated categories to sort by name

--- ember/app/models/challengeboard.js
+++ ember/app/models/challengeboard.js
@@ -1,5 +1,8 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   categories: DS.hasMany('category', {async: false}),
+  categorySorting: ['name', 'id'],
+  sortedCategories: Ember.computed.sort('categories', 'categorySorting'),
 });
