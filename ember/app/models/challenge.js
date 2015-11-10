0ebf1d09fafeaca574d84cc18d061c6537cb5558
x64x6a <darekjens@gmail.com>
2015-11-10 12:24:09 -0800

    Changed challenge solved to be hasMany of teams

--- ember/app/models/challenge.js
+++ ember/app/models/challenge.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   points: DS.attr('number'),
   description: DS.attr('string'),
-  solved: DS.attr('boolean'),
+  solved: DS.hasMany('team', {async: true}),
   num_solved: DS.attr('boolean'),
   category: DS.belongsTo('category'),
   isSolved: false,
