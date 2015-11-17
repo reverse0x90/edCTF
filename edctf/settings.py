ddef226bc39d793717e4dfebd01bf316de7c543f
Ryan Grandgenett <ryan.grandgenett@gmail.com>
2015-11-16 18:59:59 -0600

    Fixed static directory issues

--- edctf/settings.py
+++ edctf/settings.py
@@ -120,8 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'edctf/static/')
-#STATIC_ROOT = os.path.join(BASE_DIR, '/opt/edctf/edctf/static/ember')
+STATICFILES_DIRS = ('/opt/edctf/edctf/static/',)
 
 
 REST_FRAMEWORK = {
--- edctf/urls.py
+++ edctf/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
-from django.conf.urls.static import static
 from edctf.api import views
 
 urlpatterns = [
@@ -26,4 +25,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include('edctf.api.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
