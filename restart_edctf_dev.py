a2d6dec8b440a2199fd858331107fc017fd1fcf5
x64x6a <darekjens@gmail.com>
2015-11-23 20:35:58 -0800

    Added dev script to drop and recreate database

---
+++ restart_edctf_dev.py
@@ -0,0 +1,26 @@
+import os
+
+
+def main():
+    dropdb = 'sudo -u postgres psql -c "DROP DATABASE edctf;"'
+    rm_migrations = 'rm edctf/api/migrations/0*'
+    createdb = 'sudo python generate_secrets.py'
+    run_migrations = '''python manage.py makemigrations \\
+    && python manage.py migrate \\
+    && python manage.py createsuperuser
+    '''
+
+
+    choice = raw_input('This program will destroy the edctf database and delete migrations, then attempts to recreate the database\n\nARE YOU SURE? (Y/n) >')
+    print
+
+    if 'n' in choice.lower():
+        exit()
+
+    os.system(rm_migrations)
+    os.system(dropdb)
+    os.system(createdb)
+    os.system(run_migrations)
+
+if __name__ == '__main__':
+    main()
