144a572e2b4690ab3eb32c10e1175124b7935104
Ryan Grandgenett <ryan.grandgenett@gmail.com>
2015-11-28 16:34:12 -0600

    Fixed spacing issues in serializers.py

--- edctf/api/serializers.py
+++ edctf/api/serializers.py
@@ -4,36 +4,64 @@ from edctf.api.models import *
 
 
 class ctfSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ctf
-        fields = ('id', 'name', 'live', 'challengeboard', 'scoreboard')
+  """
+  Sets fields for the rest api to serialize in the ctf model.
+  """
+  class Meta:
+    model = ctf
+    fields = ('id', 'name', 'live', 'challengeboard', 'scoreboard')
+
 
 class challengeboardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = challengeboard
-        fields = ('id', 'categories')
+  """
+  Sets fields for the rest api to serialize in the challengeboard model.
+  """
+  class Meta:
+    model = challengeboard
+    fields = ('id', 'categories')
+
 
 class categorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = category
-        fields = ('id', 'name', 'challenges')
+  """
+  Sets fields for the rest api to serialize in the category model.
+  """
+  class Meta:
+    model = category
+    fields = ('id', 'name', 'challenges')
+
 
 class challengeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = challenge
-        fields = ('id', 'title', 'points', 'description', 'solved', 'numSolved', 'category')
+  """
+  Sets fields for the rest api to serialize in the challenge model.
+  """
+  class Meta:
+    model = challenge
+    fields = ('id', 'title', 'points', 'description', 'solved', 'numSolved', 'category')
+
 
 class scoreboardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = scoreboard
-        fields = ('id', 'numtopteams', 'teams')
+  """
+  Sets fields for the rest api to serialize in the scoreboard model.
+  """
+  class Meta:
+    model = scoreboard
+    fields = ('id', 'numtopteams', 'teams')
+
 
 class teamSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = team
-        fields = ('id', 'teamname', 'points', 'correct_flags', 'wrong_flags', 'solves')
+  """
+  Sets fields for the rest api to serialize in the team model.
+  """
+  class Meta:
+    model = team
+    fields = ('id', 'teamname', 'points', 'correct_flags', 'wrong_flags', 'solves')
+
 
 class challengeTimestampSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = team
-        fields = ('id','created')
+  """
+  Sets fields for the rest api to serialize in the 
+  challengeTimestamp model.
+  """
+  class Meta:
+    model = team
+    fields = ('id','created')
