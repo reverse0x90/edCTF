e9ee638ccf7c90cdd21d7a15fe8099afec21dc8d
x64x6a <darekjens@gmail.com>
2015-12-04 01:44:25 -0800

    Added development.bash script

---
+++ scripts/development.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# set working directory
+export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# set environment variables
+source ${WORKDIR}/environment.bash
+
+# attempt to destroy database
+sudo -u postgres psql -c "DROP DATABASE ${EDCTF_DATABASE};"
+
+# remove possible migration files
+rm ${EDCTF_DJANGO}/api/migrations/*initial*
+
+# run scripts
+${WORKDIR}/install_dependancies.bash \
+  && ${WORKDIR}/build_frontend.bash \
+  && ${WORKDIR}/build_backend.bash \
+  && ${WORKDIR}/start.bash
