de9a9a9eed63bc7f3c23bd38b03b75075fd17f51
x64x6a <darekjens@gmail.com>
2015-12-07 16:36:37 -0800

    Added client-side code to static folder

--- ember/config/environment.js
+++ ember/config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'ed-ctf',
+    modulePrefix: 'edctf',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
