68e5b521fcd91a0eca31240d8b77f719d2c3fb6a
x64x6a <darekjens@gmail.com>
2015-12-11 20:54:28 -0800

    Added deleted challenges route and controller

---
+++ ember/app/routes/challenges.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  setupController: function (controller){
+    controller.set('session', this.controllerFor('auth').get('session'));
+    controller.set('ctf', this.controllerFor('application').get('ctf'));
+    controller.set('modal', this.controllerFor('modal').get('modal'));
+  },
+});
