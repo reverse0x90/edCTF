0d89c0bf55581f8b4c18035f0989e7cfa9a06a91
x64x6a <darekjens@gmail.com>
2015-12-12 22:23:51 -0800

    Added 'X-CSRFToken' header generation

--- ember/app/adapters/application.js
+++ ember/app/adapters/application.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.RESTAdapter.extend({
-  namespace: 'api'
+  namespace: 'api',
+  headers: Ember.computed(function(){
+    var token = Ember.$.cookie('csrftoken');
+    return {'X-CSRFToken': token};
+  }),
 });
