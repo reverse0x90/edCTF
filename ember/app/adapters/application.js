eb1eaf65f6eef0b7ee1534fba6516555bcec5a32
x64x6a <darekjens@gmail.com>
2015-12-16 18:33:28 -0800

    Added temporary fix to hide deprecation warning

--- ember/app/adapters/application.js
+++ ember/app/adapters/application.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 
 export default DS.RESTAdapter.extend({
   namespace: 'api',
+  shouldReloadAll: function(){
+    return true;
+  },
   headers: Ember.computed(function(){
     var token = Ember.$.cookie('csrftoken');
     return {'X-CSRFToken': token};
