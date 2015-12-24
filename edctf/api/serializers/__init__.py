4da795cbedbfd4a47d04b80e73b5c35fa69a585b
x64x6a <darekjens@gmail.com>
2015-12-23 17:51:32 -0800

    Reworked api serializers

--- edctf/api/serializers/__init__.py
+++ edctf/api/serializers/__init__.py
@@ -1,75 +1,9 @@
-from rest_framework import serializers
-from edctf.api.models import *
+from serializers import CategorySerializer
+from serializers import ChallengeSerializer
+from serializers import ChallengeboardSerializer
+from serializers import ChallengeTimestampSerializer
+from serializers import CtfSerializer
+from serializers import CtftimeSerializer
+from serializers import ScoreboardSerializer
+from serializers import TeamSerializer
 import admin
-
-class CtfSerializer(serializers.ModelSerializer):
-  """
-  Sets fields for the rest api to serialize in the Ctf model.
-  """
-  class Meta:
-    model = Ctf
-    fields = ('id', 'name', 'online', 'challengeboard', 'scoreboard')
-
-
-class ChallengeboardSerializer(serializers.ModelSerializer):
-  """
-  Sets fields for the rest api to serialize in the Challengeboard model.
-  """
-  class Meta:
-    model = Challengeboard
-    fields = ('id', 'categories')
-
-
-class CategorySerializer(serializers.ModelSerializer):
-  """
-  Sets fields for the rest api to serialize in the Category model.
-  """
-  class Meta:
-    model = Category
-    fields = ('id', 'name', 'challenges')
-
-
-class ChallengeSerializer(serializers.ModelSerializer):
-  """
-  Sets fields for the rest api to serialize in the Challenge model.
-  """
-  class Meta:
-    model = Challenge
-    fields = ('id', 'title', 'points', 'description', 'solved', 'numsolved', 'category')
-
-
-class ScoreboardSerializer(serializers.ModelSerializer):
-  """
-  Sets fields for the rest api to serialize in the Scoreboard model.
-  """
-  class Meta:
-    model = Scoreboard
-    fields = ('id', 'numtopteams', 'teams')
-
-
-class TeamSerializer(serializers.ModelSerializer):
-  """
-  Sets fields for the rest api to serialize in the Team model.
-  """
-  class Meta:
-    model = Team
-    fields = ('id', 'teamname', 'points', 'correctflags', 'wrongflags', 'solves', 'lasttimestamp')
-
-
-class ChallengeTimestampSerializer(serializers.ModelSerializer):
-  """
-  Sets fields for the rest api to serialize in the
-  ChallengeTimestamp model.
-  """
-  class Meta:
-    model = ChallengeTimestamp
-    fields = ('id', 'created')
-
-
-class CtftimeSerializer(serializers.ModelSerializer):
-  """
-  Sets fields for the rest api to serialize in the team model.
-  """
-  class Meta:
-    model = Team
-    fields = ('team', 'score')
--- edctf/api/serializers/admin/__init__.py
+++ edctf/api/serializers/admin/__init__.py
@@ -1,38 +1,6 @@
-from rest_framework.serializers import ModelSerializer
-from edctf.api.models import Ctf, Challengeboard, Category, Challenge, Scoreboard, Team
-
-
-class CtfSerializer(ModelSerializer):
-  class Meta:
-    model = Ctf
-    fields = ('id', 'name', 'online', 'challengeboard', 'scoreboard')
-
-
-class ChallengeboardSerializer(ModelSerializer):
-  class Meta:
-    model = Challengeboard
-    fields = ('id', 'categories')
-
-
-class CategorySerializer(ModelSerializer):
-  class Meta:
-    model = Category
-    fields = ('id', 'name', 'challenges')
-
-
-class ChallengeSerializer(ModelSerializer):
-  class Meta:
-    model = Challenge
-    fields = ('id', 'title', 'points', 'description', 'solved', 'numsolved', 'category')
-
-
-class ScoreboardSerializer(ModelSerializer):
-  class Meta:
-    model = Scoreboard
-    fields = ('id', 'numtopteams', 'teams')
-
-
-class TeamSerializer(ModelSerializer):
-  class Meta:
-    model = Team
-    fields = ('id', 'teamname', 'points', 'correctflags', 'wrongflags', 'solves', 'lasttimestamp')
+from serializers import CategorySerializer
+from serializers import ChallengeSerializer
+from serializers import ChallengeboardSerializer
+from serializers import CtfSerializer
+from serializers import ScoreboardSerializer
+from serializers import TeamSerializer
