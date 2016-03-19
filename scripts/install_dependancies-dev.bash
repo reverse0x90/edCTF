0c5f35c436a34b5035cc831d36044c7e3cdad81f
x64x6a <darekjens@gmail.com>
2016-03-18 20:49:12 -0500

    Updated dev installation

--- scripts/install_dependancies-dev.bash
+++ scripts/install_dependancies-dev.bash
@@ -9,11 +9,11 @@ sudo apt-get update \
     git \
     libapache2-mod-wsgi \
     libpq-dev \
+    nodejs \
     postgresql \
     python-pip \
     python-dev \
-  && sudo pip install -r ${EDCTF_SCRIPTS}/requirements.txt \
-  && (curl -sL https://deb.nodesource.com/setup | sudo bash -) \
-  && sudo apt-get install -y nodejs \
+  && pip install -r ${EDCTF_SCRIPTS}/requirements.txt \
+  && (sudo npm cache clean -f && sudo npm install -g n && sudo n stable) \
   && sudo npm install -g ember-cli \
   && sudo npm install -g bower
