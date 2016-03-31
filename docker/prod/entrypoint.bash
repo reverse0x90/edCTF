aece3277b1f959581e195e6d080e91f926e98170
x64x6a <darekjens@gmail.com>
2016-03-30 22:37:05 -0500

    Added docker-compose

--- docker/prod/entrypoint.bash
+++ docker/prod/entrypoint.bash
@@ -2,7 +2,9 @@
 set -x
 
 # Start services
-/etc/init.d/postgresql start && /usr/sbin/apache2ctl -k restart
+#/etc/init.d/postgresql start && /usr/sbin/apache2ctl -k restart
+/etc/init.d/postgresql start
 
 # Entrypoint
-/bin/bash
+#sudo -u postgres /usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf
+/usr/sbin/apache2ctl -D FOREGROUND
