aef182a5cc3a25fe56bc67a2768abaf5e11bfb29
x64x6a <darekjens@gmail.com>
2016-03-31 00:37:07 -0500

    Moved backend build to entrypoint

--- docker/prod/entrypoint.bash
+++ docker/prod/entrypoint.bash
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -x
 
+. ${SCRIPTS}/environment.bash
+
 # Start services
-#/etc/init.d/postgresql start && /usr/sbin/apache2ctl -k restart
 /etc/init.d/postgresql start
 
+# Build backend
+${SCRIPTS}/build_backend.bash
+
 # Entrypoint
-#sudo -u postgres /usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf
 /usr/sbin/apache2ctl -D FOREGROUND
