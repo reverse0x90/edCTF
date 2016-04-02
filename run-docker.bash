dcd867e64f111a637ee65aaa31c5f5f432923194
x64x6a <darekjens@gmail.com>
2016-04-02 02:59:38 -0500

    Added local option to run-docker

--- run-docker.bash
+++ run-docker.bash
@@ -26,27 +26,39 @@ if $DEV && $PROD; then
   exit 1
 fi
 
-if $DEV; then
-  UUID=`id -u`
-  echo "Creating development container..."
-  set -x
-  docker build -t edctf:dev -f ${EDCTF_DOCKER}/dev/Dockerfile ${EDCTF_DIR} \
-    && docker run --name edctf_db_dev \
-      -e POSTGRES_USER=edctf -e POSTGRES_PASSWORD=edctf \
-      -d postgres \
-    && docker run --restart=unless-stopped --link edctf_db_dev:db \
-      -e UUID=$UUID -e USER=$USER \
-      -v ${EDCTF_DIR}:/opt/edctf \
-      -p 8080:80 -p 4443:443 \
-      -it edctf:dev
+if ! $LOCAL; then
+  if $DEV; then
+    UUID=`id -u`
+    echo "Creating development container..."
+    set -x
+    docker build -t edctf:dev -f ${EDCTF_DOCKER}/dev/Dockerfile ${EDCTF_DIR} \
+      && docker run --name edctf_db_dev \
+        -e POSTGRES_USER=edctf -e POSTGRES_PASSWORD=edctf \
+        -d postgres \
+      && docker run --restart=unless-stopped --link edctf_db_dev:db \
+        -e UUID=$UUID -e USER=$USER \
+        -v ${EDCTF_DIR}:/opt/edctf \
+        -p 8080:80 -p 4443:443 \
+        -it edctf:dev
+  else
+    echo "Creating production container..."
+    set -x
+    docker build -t edctf:prod ${EDCTF_DIR} \
+      && docker run --name edctf_db \
+        -e POSTGRES_USER=edctf -e POSTGRES_PASSWORD=edctf \
+        -d postgres \
+      && docker run --restart=unless-stopped --name=edctf_server --link edctf_db:db \
+        -p 80:80 -p 443:443 \
+        -d edctf:prod
+  fi
 else
-  echo "Creating production container..."
-  set -x
-  docker build -t edctf:prod ${EDCTF_DIR} \
-    && docker run --name edctf_db \
-      -e POSTGRES_USER=edctf -e POSTGRES_PASSWORD=edctf \
-      -d postgres \
-    && docker run --restart=unless-stopped --name=edctf_server --link edctf_db:db \
-      -p 80:80 -p 443:443 \
-      -d edctf:prod
+  if $DEV; then
+    echo "Creating development environment locally..."
+    set -x
+    ${EDCTF_DIR}/scripts/development.bash
+  else
+    echo "Creating production environment locally..."
+    set -x
+    ${EDCTF_DIR}/scripts/production.bash
+  fi
 fi
