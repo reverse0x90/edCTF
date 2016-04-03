a1b95ddc386608b84dd26ed3b248a07eaf918e46
x64x6a <darekjens@gmail.com>
2016-04-02 21:15:30 -0500

    Removed unused scripts

--- scripts/build_frontend-prod.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Generates client-side items, including building ember and copying CSS
-
-cp -R ${DJANGO_ADMIN_STATIC} ${EDCTF_ADMIN_STATIC} \
-  && cp -R ${REST_FRAMEWORK_CSS_DIR} ${EDCTF_REST_STATIC}
