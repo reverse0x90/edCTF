a00d978e08926297bd2be5633c59fe0243ade17d
x64x6a <darekjens@gmail.com>
2016-04-19 19:57:18 -0700

    Fixed home and about routes to use ctf.home and ctf.about

--- ember/app/controllers/about.js
+++ ember/app/controllers/about.js
@@ -2,14 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   ctf: null,
-  html: '',
-  set_html: function(){
-    var ctf = this.get('ctf');
-    if(ctf){
-      var t = this;
-      ctf.get('about').then(function(about){
-        t.set('html', about.get('html'));
-      });
-    }
-  }.observes('ctf').on('init'),
 });
--- ember/app/controllers/home.js
+++ ember/app/controllers/home.js
@@ -2,14 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   ctf: null,
-  html: '',
-  set_html: function(){
-    var ctf = this.get('ctf');
-    if(ctf){
-      var t = this;
-      ctf.get('home').then(function(home){
-        t.set('html', home.get('html'));
-      });
-    }
-  }.observes('ctf').on('init'),
 });
