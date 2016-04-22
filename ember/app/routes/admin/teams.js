55408c5d7c069bd63bec3cbf1046e8dc87087fe5
x64x6a <darekjens@gmail.com>
2016-04-21 23:02:18 -0500

    Added teams table to admin/teams route

--- ember/app/routes/admin/teams.js
+++ ember/app/routes/admin/teams.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  setupController: function (controller){
+  model: function(){
+    return this.store.findAll('team');
+  },
+  setupController: function (controller, model){
+    controller.set('teams', model);
     controller.set('session', this.controllerFor('auth').get('session'));
     controller.set('ctf', this.controllerFor('application').get('ctf'));
     controller.set('modal', this.controllerFor('modal').get('modal'));
